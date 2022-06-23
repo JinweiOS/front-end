e9c09d5f76366a22c9524ca0b07386a92067b3ea
jinweiOS <jinwei.peng@outlook.com>
2022-06-23 16:00:37 +0800

    信管无敌

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="信管无敌，女生可爱，男生帅气"/>
   </div>
 </template>
 
